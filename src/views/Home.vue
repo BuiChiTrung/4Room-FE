7c827d55ea4452ec9b47b13b94c5df380fec6dc5
BuiChiTrung <trungcspntl@gmail.com>
2021-10-18 16:54:22 +0700

    add vue project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,17 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app">
+      <!-- ACCESS DATA -->
+      <div v-if="showFilms">{{ title }} - {{ mainActor }} - {{ publishYear }}</div>
+
+      <!-- CLICK EVENT -->
+      <!-- onclick function can be declared inside html or in vue js file -->
+      <button v-on:click="publishYear++">Increase Year</button>
+      <button v-on:click="publishYear--">Decrease Year</button>
+      <!-- @click is short term for v-on:click -->
+      <br>
+    </div>
   </div>
 </template>
 
@@ -13,6 +24,23 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      showFilms: true,
+      title: 'Pirate of caribeen',
+      mainActor: 'Johny Deep',
+      publishYear: 2012
+    }
+  },
+  computed: {
+    a: 10,
   }
 }
 </script>
+
+<style lang="scss">
+  button {
+    background: blue;
+  }
+</style>
