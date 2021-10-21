8a44e98bc53dfb4eb91b4e6291c92c570d127d83
ngot23 <levuquang2k1@gmail.com>
2021-10-22 01:16:02 +0700

    (+) add UpPost component with avatar and textarea. Need to handle uploading file to server

---
+++ src/components/newsfeed/UpPost.vue
@@ -0,0 +1,98 @@
+<template>
+<div class="up-post container">
+  <div class="row row-with-caption">
+    <div class="col-2">
+      <a href="#">
+        <img id="avatar" src="@/assets/images/icons/avatar.png" alt="avatar">
+      </a>
+    </div>
+    <div class="col-10 div-write-caption">
+      <textarea class="form-control text-write-caption" name="caption" placeholder="what's on your mind?">
+      </textarea>
+    </div>
+  </div>
+
+  <div class="row attach-file">
+<!--    <div class="col-10 d-flex mb-3">-->
+<!--      <div class="p-2 bd-highlight">-->
+<!--        <form name="uploadForm">-->
+<!--          <input id="uploadInput" type="file" name="myFiles" multiple>-->
+<!--          <div><input type="submit" value="Send file"></div>-->
+<!--        </form>-->
+<!--      </div>-->
+<!--      <div class="ms-auto p-2 bd-highlight">Flex item</div>-->
+<!--    </div>-->
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "UpPost"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/sass/style";
+
+/* dev */
+//div * {
+//  border: 1px black solid;
+//}
+
+.up-post {
+  margin-bottom: 1em;
+  border: 1px solid #D0D4D9;
+  border-radius: 12px;
+  padding-top: 2.5em;
+  padding-bottom: 2.5em;
+}
+
+.text-write-caption {
+  font-size: 12.9px;
+  line-height: 1.2;
+}
+
+.row-with-caption {
+  height: 7.7em;
+
+}
+
+.attach-file {
+  margin-top: 1em;
+}
+
+.div-write-caption {
+  display: inline;
+}
+
+#avatar {
+  height: 5.5em;
+}
+
+textarea {
+  height: 100%;
+  resize: none;
+  border: 1px solid #D0D4D9;
+  border-radius: 12px;
+}
+
+textarea::-webkit-scrollbar {
+  width: 9px;
+  background-color: #ffffff;
+}
+
+textarea::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  background-color: #b5c3da;
+}
+
+$breakpoint-tablet: 500px;
+@media (max-width: $breakpoint-tablet) {
+  #avatar {
+    display: none;
+  }
+}
+
+
+</style>
--- src/views/home/HomePage.vue
+++ src/views/home/HomePage.vue
@@ -1,24 +1,67 @@
 <template>
   <NavBar bgColor="white"/>
   <SideBar bgColor="white"/>
-  <main>
-    <div>Hello world</div>
+
+  <main class="container w-100 vh-100 p-3">
+    <div class="row justify-content-center">
+      <div class="col-8 container newsfeed">
+        <div class="row justify-content-center">
+          <div class="col-10 container">
+            <UpPost></UpPost>
+            <Posts></Posts>
+          </div>
+        </div>
+      </div>
+
+      <div class="follow col-4">
+        follow
+        <Follow></Follow>
+      </div>
+    </div>
+
   </main>
 </template>
 
 <script>
 import SideBar from "@/components/layout/SideBar";
 import NavBar from "@/components/layout/NavBar";
+import UpPost from "../../components/newsfeed/UpPost";
+import Posts from "../../components/newsfeed/Posts";
+import Follow from "../../components/newsfeed/Follow";
+
 export default {
   name: "HomePage",
-  components: {NavBar, SideBar}
+  components: {
+    NavBar,
+    SideBar,
+    UpPost,
+    Posts,
+    Follow
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 @import "../../assets/sass/style";
+
+/* dev */
+//main, main * {
+//  border: 1px solid black;
+//}
+
 main {
-  padding-left: 2rem;
-  //background: green;
+  margin-top: 2em;
 }
+
+$breakpoint-tablet: 775px;
+@media (max-width: $breakpoint-tablet) {
+  .follow {
+    display: none;
+  }
+
+  .newsfeed {
+    width: 100%;
+  }
+}
+
 </style>
