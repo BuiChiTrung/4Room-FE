8e1cb96dab3247440972cef043401d0e17f9ff5b
ngot23 <levuquang2k1@gmail.com>
2021-11-06 18:32:27 +0700

    (+) fix comments

--- src/views/profile/ProfilePage.vue
+++ src/views/profile/ProfilePage.vue
@@ -3,74 +3,74 @@
   <SideBar bgColor="rgba(223, 249, 251,0.2)"/>
 
   <main>
-    <div id="profile">
-      <img alt="avatar" src="@/assets/images/icons/avatar.png">
-      <div v-if="showEditForm" id="profile-static">
-        <div>{{userInfo.name_in_forum}}</div>
-        <div>{{userInfo.bio}}</div>
-        <hr>
-        <button class="btn btn-info" @click="toggleEditForm">Edit Profile</button>
+      <div id="profile" >
+        <img alt="avatar" src="@/assets/images/icons/avatar.png">
+        <div v-if="showEditForm" id="profile-static">
+          <div>{{userInfo.name_in_forum}}</div>
+          <div>{{userInfo.bio}}</div>
+          <hr>
+          <button class="btn btn-info" @click="toggleEditForm">Edit Profile</button>
 
-        <div id="contact">
-          <a :href="userInfo.linkedin_link"><i class="fab fa-linkedin"></i></a>
-          <a :href="userInfo.instagram_link"><i class="fab fa-instagram"></i></a>
-          <a :href="userInfo.facebook_link"><i class="fab fa-facebook"></i></a>
-          <a :href="userInfo.twitter_link"><i class="fab fa-twitter"></i></a>
+          <div id="contact">
+            <a :href="userInfo.linkedin_link"><i class="fab fa-linkedin"></i></a>
+            <a :href="userInfo.instagram_link"><i class="fab fa-instagram"></i></a>
+            <a :href="userInfo.facebook_link"><i class="fab fa-facebook"></i></a>
+            <a :href="userInfo.twitter_link"><i class="fab fa-twitter"></i></a>
+          </div>
         </div>
+
+        <form v-else id="edit-form">
+          <div>
+            <label for="name_in_forum">Name</label>
+            <input type="text" class="form-control" id="name_in_forum" v-model="userInfo.name_in_forum">
+          </div>
+
+          <div>
+            <label for="bio">Bio</label>
+            <textarea type="email" class="form-control" id="bio" aria-describedby="emailHelp" v-model="userInfo.bio"></textarea>
+          </div>
+
+          <hr>
+
+          <div class="row">
+            <label class="col-1 col-form-label" for="linkedin-link" ><i class="fab fa-linkedin"></i></label>
+            <div class="col-11">
+              <input class="form-control" id="linkedin-link" name="linkedin-link" placeholder="Linkedin Link" type="text" v-model="userInfo.linkedin_link">
+            </div>
+          </div>
+
+          <div class="row">
+            <label class="col-1 col-form-label" for="instagram-link" ><i class="fab fa-instagram"></i></label>
+            <div class="col-11">
+              <input class="form-control" id="instagram-link" name="instagram-link" placeholder="Instagram Link" type="text" v-model="userInfo.instagram_link">
+            </div>
+          </div>
+
+          <div class="row">
+            <label class="col-1 col-form-label" for="facebook-link" ><i class="fab fa-facebook"></i></label>
+            <div class="col-11">
+              <input class="form-control" id="facebook-link" name="linkedin-link" placeholder="Facebook Link" type="text" v-model="userInfo.facebook_link">
+            </div>
+          </div>
+
+          <div class="row">
+            <label class="col-1 col-form-label" for="twitter-link" ><i class="fab fa-twitter"></i></label>
+            <div class="col-11">
+              <input class="form-control" id="twitter-link" name="linkedin-link" placeholder="Twitter Link" type="text" v-model="userInfo.twitter_link">
+            </div>
+          </div>
+
+          <div id="buttons">
+            <input type="submit" class="btn btn-info btn-save"  value="Save">
+            <button class="btn btn-outline-danger btn-cancel" @click="toggleEditForm">Cancel</button>
+          </div>
+        </form>
       </div>
 
-      <form v-else id="edit-form">
-        <div>
-          <label for="name_in_forum">Name</label>
-          <input type="text" class="form-control" id="name_in_forum" v-model="userInfo.name_in_forum">
-        </div>
+      <div id="gege">
+        <h1>hello</h1>
 
-        <div>
-          <label for="bio">Bio</label>
-          <textarea type="email" class="form-control" id="bio" aria-describedby="emailHelp" v-model="userInfo.bio"></textarea>
-        </div>
-
-        <hr>
-
-        <div class="row">
-          <label class="col-1 col-form-label" for="linkedin-link" ><i class="fab fa-linkedin"></i></label>
-          <div class="col-11">
-            <input class="form-control" id="linkedin-link" name="linkedin-link" placeholder="Linkedin Link" type="text" v-model="userInfo.linkedin_link">
-          </div>
-        </div>
-
-        <div class="row">
-          <label class="col-1 col-form-label" for="instagram-link" ><i class="fab fa-instagram"></i></label>
-          <div class="col-11">
-            <input class="form-control" id="instagram-link" name="instagram-link" placeholder="Instagram Link" type="text" v-model="userInfo.instagram_link">
-          </div>
-        </div>
-
-        <div class="row">
-          <label class="col-1 col-form-label" for="facebook-link" ><i class="fab fa-facebook"></i></label>
-          <div class="col-11">
-            <input class="form-control" id="facebook-link" name="linkedin-link" placeholder="Facebook Link" type="text" v-model="userInfo.facebook_link">
-          </div>
-        </div>
-
-        <div class="row">
-          <label class="col-1 col-form-label" for="twitter-link" ><i class="fab fa-twitter"></i></label>
-          <div class="col-11">
-            <input class="form-control" id="twitter-link" name="linkedin-link" placeholder="Twitter Link" type="text" v-model="userInfo.twitter_link">
-          </div>
-        </div>
-
-        <div id="buttons">
-          <input type="submit" class="btn btn-info btn-save"  value="Save">
-          <button class="btn btn-outline-danger btn-cancel" @click="toggleEditForm">Cancel</button>
-        </div>
-
-      </form>
-    </div>
-
-    <div id="posts">
-
-    </div>
+      </div>
   </main>
 </template>
 
@@ -115,15 +115,15 @@ main {
 }
 
 #profile {
-  width: 50rem;
+  height: 20rem;
   font-size: 2rem;
   padding: 8rem;
-  min-height: 100vh;
-  //background: yellow;
+  min-height: 30em;
   background-image: linear-gradient(90deg, rgba(223, 249, 251, 0.8),rgba(248, 255, 255,0.8)), url('../../assets/images/background-1.jpg');
+
   background-size: cover;
   img {
-    width: 80%;
+    width: 10%;
     display: block; margin: auto;
   }
   #profile-static {
@@ -165,4 +165,10 @@ main {
     }
   }
 }
+
+.no-left-right-margin {
+  //margin-left: 0;
+  //margin-right: 0;
+}
+
 </style>
