4c51a9867b00401d821c6e598c8433c134110c82
BuiChiTrung <trungcspntl@gmail.com>
2021-12-04 20:34:57 +0700

    useless commit

--- src/env/index.js
+++ src/env/index.js
@@ -1,7 +1,7 @@
 // dev
-// export const baseApiUrl = 'http://localhost:8000/api'
-// export const baseBroadcastUrl = 'http://localhost:8000';
+export const baseApiUrl = 'http://localhost:8000/api'
+export const baseBroadcastUrl = 'http://localhost:8000';
 
 // production
-export const baseApiUrl = 'http://4room.com.vn/api';
-export const baseBroadcastUrl = 'http://4room.com.vn';
+// export const baseApiUrl = 'http://4room.com.vn/api';
+// export const baseBroadcastUrl = 'http://4room.com.vn';
