9cc9dbd7554379f450a518b91ca382e492561ff6
BuiChiTrung <trungcspntl@gmail.com>
2021-12-12 18:20:13 +0700

    test md editor

---
+++ src/views/post/PostEditor.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-md-editor v-model="text" height="400px"></v-md-editor>
+  <v-md-preview :text="text"></v-md-preview>
+</template>
+
+<script>
+
+export default {
+  name: "PostEditor",
+  data() {
+    return {
+      text: '',
+    };
+  },
+  watch: {
+    text() {
+      console.log(this.text);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
