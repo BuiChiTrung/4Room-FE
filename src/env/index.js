55aa193dd3d7caeffd257aa0a80ebece09cd82aa
BuiChiTrung <trungcspntl@gmail.com>
2021-12-13 11:55:46 +0700

    add env file

---
+++ src/env/index.js
@@ -0,0 +1,7 @@
+const baseUrl = 'http://localhost:8000';
+//const baseUrl = 'http://4room.com.vn';
+//const baseUrl = 'http://4room.tech';
+
+export const baseApiUrl = `${baseUrl}/api`;
+export const baseBroadcastUrl = `${baseUrl}/broadcasting/auth`;
+export const baseStorageAPI = `${baseUrl}/storage`;
